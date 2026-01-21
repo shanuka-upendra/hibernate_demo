beecabedb00abf7e2729b4dd368931ecbe8a1eed
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-01-21 14:24:28 +0530

    feat : ddl-auto create change into update and get Transaction.

--- src/main/java/edu/icet/Main.java
+++ src/main/java/edu/icet/Main.java
@@ -2,15 +2,16 @@ package edu.icet;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 
 public class Main {
     public static void main(String[] args) {
         Student student = new Student();
-        student.setStudentId(1);
-        student.setName("Shanuu");
-        student.setAge(19);
+        student.setStudentId(2);
+        student.setName("Qwerty");
+        student.setAge(50);
 
         Configuration config = new Configuration();
         config.addAnnotatedClass(edu.icet.Student.class);
@@ -18,6 +19,11 @@ public class Main {
 
         SessionFactory factory = config.buildSessionFactory();
         Session session = factory.openSession();
+
+        Transaction transaction = session.beginTransaction();
         session.persist(student);
+        transaction.commit();
+
+
     }
 }
