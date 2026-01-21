bdbe39b74f03173964262301e74f1442967fcd47
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-01-21 13:46:54 +0530

    Initial commit

---
+++ src/main/java/edu/icet/Main.java
@@ -0,0 +1,13 @@
+package edu.icet;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/edu/icet/Student.java
@@ -0,0 +1,31 @@
+package edu.icet;
+
+public class Student {
+    private Integer studentId;
+    private String name;
+    private int age;
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
