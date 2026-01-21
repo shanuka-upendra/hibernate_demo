7abb241c85c451de997d00a70f874cb7ad7ca316
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-01-21 13:48:33 +0530

    feat : add configuration and create xml.

--- src/main/java/edu/icet/Main.java
+++ src/main/java/edu/icet/Main.java
@@ -7,7 +7,16 @@ import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
+        Student student = new Student();
+        student.setStudentId(1);
+        student.setName("Shanuu");
+        student.setAge(19);
 
+        Configuration config = new Configuration();
+        config.configure("hibernate.cfg.xml");
 
+        SessionFactory factory = config.buildSessionFactory();
+        Session session = factory.openSession();
+        session.persist(student);
     }
 }
